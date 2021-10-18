e114de3eb21c85dfb792e02e6a405ec61ba93b50
UCT <vinhtq279@gmail.com>
2021-10-18 22:06:04 +0700

    Install Flask-Bootstrap

--- hello.py
+++ hello.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return '<h1>Hello World!</h1>'
+	return render_template('index.html')
 
-@app.route('/user/<int:id>')
-def user(id):
-	return '<h1>Hello {}</h1>'.format(id)
+@app.route('/user/<name>')
+def user(name):
+	return render_template('user.html', name=name)
