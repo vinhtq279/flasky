1d69c227908d96292711445d3a210adcd5e21433
UCT <vinhtq279@gmail.com>
2021-10-20 14:45:29 +0700

    .

--- hello.py
+++ hello.py
@@ -1,11 +1,21 @@
 from flask_bootstrap import Bootstrap
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
 app = Flask(__name__)
 Bootstrap(app)
+app.config['SECRET_KEY'] = 'hard to guess string'
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-	return render_template('index.html')
+	name = None
+	form = NameForm()
+	if form.validate_on_submit():
+		name = form.name.data
+		form.name.data = ''
+	return render_template('index.html', form = form, name = name)
 
 @app.route('/user/<name>')
 def user(name):
@@ -14,3 +24,7 @@ def user(name):
 @app.route('/base1/')
 def base1():
 	return render_template('base1.html')
+
+class NameForm(FlaskForm):
+	name = StringField('What is your name?', validators=[DataRequired()])
+	submit = SubmitField('Submit')
