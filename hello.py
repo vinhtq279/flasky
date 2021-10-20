48b822c99648f421cd93fd94ddf9b19b36700a18
Vinh Trinh Quang <vinhtq279@gmail.com>
2021-10-20 10:51:03 +0700

    .

--- hello.py
+++ hello.py
@@ -1,5 +1,7 @@
+from flask_bootstrap import Bootstrap
 from flask import Flask, render_template
 app = Flask(__name__)
+Bootstrap(app)
 
 @app.route('/')
 def index():
@@ -8,3 +10,7 @@ def index():
 @app.route('/user/<name>')
 def user(name):
 	return render_template('user.html', name=name)
+
+@app.route('/base1/')
+def base1():
+	return render_template('base1.html')
