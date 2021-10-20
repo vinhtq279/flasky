afe24abc47f20fa905046177d0fbc3fb6ca07486
Vinh Trinh Quang <vinhtq279@gmail.com>
2021-10-20 15:45:46 +0700

    .

--- hello.py
+++ hello.py
@@ -1,8 +1,9 @@
 from flask_bootstrap import Bootstrap
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask import session, flash
 
 app = Flask(__name__)
 Bootstrap(app)
@@ -13,9 +14,12 @@ def index():
 	name = None
 	form = NameForm()
 	if form.validate_on_submit():
-		name = form.name.data
-		form.name.data = ''
-	return render_template('index.html', form = form, name = name)
+		old_name = session.get('name')
+		if old_name is not None and old_name != form.name.data:
+			flash('Looks like you have changed your name!')
+		session['name'] = form.name.data
+		return redirect(url_for('index'))
+	return render_template('index.html', form=form, name=session.get('name'))
 
 @app.route('/user/<name>')
 def user(name):
